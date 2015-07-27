ca0292437719bcf357754c294c78b3c72b86be37
Thecrazyskull <anaskarbila@gmail.com>
2015-07-27 02:28:08 +0200

    Initial cm lollipop device tree

---
+++ cm.mk
@@ -0,0 +1,13 @@
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/kylessopen/kylessopen.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := kylessopen
+PRODUCT_NAME := cm_kylessopen
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := GT-S7560M
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_RELEASE_NAME := GT-S7560M
