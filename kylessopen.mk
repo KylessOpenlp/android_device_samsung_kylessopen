3a6ad493c27b37bdcc8e9ba2481cbbe449d5bdea
Thecrazyskull <anaskarbila@gmail.com>
2015-07-27 13:51:48 +0200

    kylessopen: use libstagefright-plugins

--- kylessopen.mk
+++ kylessopen.mk
@@ -52,6 +52,13 @@ PRODUCT_PACKAGES += \
     LiveWallpapersPicker \
     librs_jni
 
+# Media
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml
+
 # Power HAL
 PRODUCT_PACKAGES += \
     power.msm7x27a
